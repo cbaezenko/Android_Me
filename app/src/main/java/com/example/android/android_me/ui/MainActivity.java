dde6df55425a485e73cae49505ff6fb7da5de6b0
Singorenko <cbaezenko@gmail.com>
2018-03-31 13:19:13 +0300

    result 04

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+}
